fab1078d69c2c0b31c0cf0f42a2adcdeca097e2b
TryConor <cjmw5t@umsystem.edu>
2023-11-04 13:11:04 -0500

    Update Health.cs

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public int health = 100;
+    public int mana = 50;
+    private int experience = 0;
     // Start is called before the first frame update
     void Start()
     {
