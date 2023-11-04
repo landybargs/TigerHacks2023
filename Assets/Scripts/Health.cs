61935aad301a150ad356d60fd26dae28785a5ac4
TryConor <cjmw5t@umsystem.edu>
2023-11-04 13:36:29 -0500

    NewScenes + Health + Mouseinputs script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField]public int health = 100;
-    [SerializeField]public int mana = 50;
+    public int health = 100;
+    public int mana = 50;
     [SerializeField]private int experience = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            health -= 20;
+            if(health <= 0)
+            {
+                Debug.Log("You Died");
+            }
+        }
     }
 }
